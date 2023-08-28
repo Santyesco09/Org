3075dd6dc350fef6db1f92f65fa54b3ff1e2e66a
Santy <santiagoescobarcorredor@gmail.com>
2023-08-28 12:07:51 -0500

    Segundo Curso de React

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,46 +1,56 @@
 import { useState } from "react";
 import "./Formulario.css";
-import CampoTexto from "../CampoTexto";
+import Campo from "../Campo";
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const [nombre, setNombre] = useState("");
     const [puesto, setPuesto] = useState("");
     const [foto, setFoto] = useState("");
     const [equipo, setEquipo] = useState("");
 
+    const [titulo, setTitulo] = useState("");
+    const [color, setColor] = useState("");
+
+    const { registrarColaborador, crearEquipo } = props;
+
     const manejarEnvio = (e) => {
         e.preventDefault();
         console.log("Manejar el envio");
         let datosEnviar = {
-            nombre,
-            puesto,
-            foto,
-            equipo
+            nombre: nombre,
+            puesto: puesto,
+            foto: foto,
+            equipo: equipo
         }
-        console.log(datosEnviar);
+        props.registrarColaborador(datosEnviar);
+    }
+
+    const manejarNuevoEquipo = (e) => {
+        e.preventDefault();
+        crearEquipo({ titulo, colorPrimario: color });
     }
 
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto
+            <Campo
                 titulo="Nombre"
                 placeholder="Ingresar nombre"
                 required={true}
                 valor={nombre}
                 setValor={setNombre}
             />
-            <CampoTexto
+            <Campo
                 titulo="Puesto"
                 placeholder="Ingresar puesto"
                 required
                 valor={puesto}
                 setValor={setPuesto}
             />
-            <CampoTexto
+            <Campo
                 titulo="Foto"
                 placeholder="Ingresar enlace de foto"
                 required
@@ -50,9 +60,31 @@ const Formulario = () => {
             <ListaOpciones
                 valor={equipo}
                 setEquipo={setEquipo}
+                equipos={props.equipos}
             />
             <Boton>
                 Crear
+            </Boton>
+        </form>
+        <form onSubmit={manejarNuevoEquipo}>
+            <h2>Rellena el formulario para crear el equipo.</h2>
+            <Campo
+                titulo="Titulo"
+                placeholder="Ingresar titulo"
+                required={true}
+                valor={titulo}
+                setValor={setTitulo}
+            />
+            <Campo
+                titulo="Color"
+                placeholder="Ingresar el color Hex"
+                required
+                valor={color}
+                setValor={setColor}
+                type="color"
+            />
+            <Boton>
+                Registrar Equipo
             </Boton>
         </form>
     </section>
--- src/components/ListaOpciones/index.js
+++ src/components/ListaOpciones/index.js
@@ -2,19 +2,6 @@ import "./ListaOpciones.css";
 
 const ListaOpciones = (props) => {
 
-    /* Metodo map -> arreglo.map( (equipo, index) => {
-        return <option><option/>
-    })*/
-    const equipos = [
-        "Programación",
-        "Front End",
-        "Data Science",
-        "Devops",
-        "UX y Diseño",
-        "Móvil",
-        "Innovación y Gestión"
-    ];
-
     const manejarCambio = (e) => {
         console.log("Cambio", e.target.value);
         props.setEquipo(e.target.value);
@@ -23,13 +10,9 @@ const ListaOpciones = (props) => {
     return <div className="lista-opciones">
         <label>Equipos</label>
         <select value={props.valor} onChange={manejarCambio}>
-            <option
-                value=""
-                disabled
-                defaultValue=""
-                hidden
+            <option value="" disabled defaultValue="" hidden
             >Seleccionar equipo</option>
-            {equipos.map((equipo, index) =>
+            {props.equipos.map((equipo, index) =>
                 <option key={index} value={equipo}>{equipo}</option>
             )}
         </select>
